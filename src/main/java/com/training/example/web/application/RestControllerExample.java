40daff8e82c7d556ed0811014a2a785c6bad7b0e
divang <divang.s@gmail.com>
2018-10-28 02:23:39 +0530

    Added Spring REST template example

--- src/main/java/com/training/example/web/application/RestControllerExample.java
+++ src/main/java/com/training/example/web/application/RestControllerExample.java
@@ -1,13 +1,26 @@
 package com.training.example.web.application;
 
+import com.training.example.web.application.entities.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RestControllerExample {
 
-    @RequestMapping("/")
+    private static final Logger log = LoggerFactory.getLogger(RestControllerExample.class);
+    @RequestMapping("/home")
     public String home(){
         return "It is Spring REST Controller";
+    }
+
+    @RequestMapping("/restapi")
+    public String restAPICall() {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info("REST API response:"+quote);
+        return quote.toString();
     }
 }
